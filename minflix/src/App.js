6f0a11c88b9ea8a4f01177d1fe2255a6608508b3
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2024-01-03 00:53:59 +0900

    practice components and props

--- minflix/src/App.js
+++ minflix/src/App.js
@@ -1,12 +1,14 @@
-function Potato() {
-  return <h3>I like a potato</h3>;
+function Food({fav}) {
+  return <h3>I like { fav }.</h3>;
 }
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      <Potato />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chocolate" />
     </div>
   );
 }
