a121f266d2093c268f7e98e8f60dbd58087f6f9a
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2024-01-04 17:16:14 +0900

    load data from movie api

--- minflix/src/App.js
+++ minflix/src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    // load movie data
+    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    console.log(movies);
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
   render() {
-    return <div />;
+    const { isLoading } = this.state;
+    return (
+      <div>
+        {isLoading ? 'Loading...' : 'We are ready'}
+      </div>
+    );
   }
 }
 
