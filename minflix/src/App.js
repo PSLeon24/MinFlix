4439c68efe18c7b463fc60ca8554bb139d193831
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2024-01-04 16:42:58 +0900

    Update App.js

--- minflix/src/App.js
+++ minflix/src/App.js
@@ -1,18 +1,40 @@
 import React from 'react';
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello constructor');
+  }
+
+  componentDidMount(){
+    console.log('hello componentDidMount');
+  }
+
+  componentDidUpdate(){
+    console.log('hello componentDidUpdate');
+  }
+
+  componentWillUnmount(){
+    console.log('hello componentWillUnmount');
+  }
+
   state = {
     count: 0,
   };
 
   add = () => {
-    console.log('add');
+    this.setState(current => ({
+      count: current.count + 1,
+    }));
   }
 
-  minus() {
-    console.log('minus');
-  }
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1,
+    }));
+  };
 
   render() {
+    console.log('hello render');
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
