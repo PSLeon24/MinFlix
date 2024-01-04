1c96b0d733b1c6b712240abc27f703ad853d2731
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2024-01-04 16:49:00 +0900

    clean up my code

--- minflix/src/App.js
+++ minflix/src/App.js
@@ -1,47 +1,7 @@
 import React from 'react';
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('hello constructor');
-  }
-
-  componentDidMount(){
-    console.log('hello componentDidMount');
-  }
-
-  componentDidUpdate(){
-    console.log('hello componentDidUpdate');
-  }
-
-  componentWillUnmount(){
-    console.log('hello componentWillUnmount');
-  }
-
-  state = {
-    count: 0,
-  };
-
-  add = () => {
-    this.setState(current => ({
-      count: current.count + 1,
-    }));
-  }
-
-  minus = () => {
-    this.setState(current => ({
-      count: current.count - 1,
-    }));
-  };
-
   render() {
-    console.log('hello render');
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    return <div />;
   }
 }
 
