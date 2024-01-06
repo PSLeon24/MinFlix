1ec0a8aa586090857f9035d8733163b37744301e
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2024-01-06 22:45:09 +0900

    create a navigation

--- minflix/src/App.js
+++ minflix/src/App.js
@@ -1,60 +1,19 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
 import './App.css';
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Navigation from './components/Navigation';
 
-  getMovies = async () => {
-    // load movie data
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    this.setState({ movies, isLoading: false });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        <header className="header">
-        <h1 className="header__title">MinFlix</h1>
-        <hr />
-      </header>
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key = {movie.id}
-                id = {movie.id}
-                year = {movie.year}
-                title = {movie.title}
-                summary = {movie.summary}
-                poster = {movie.medium_cover_image}
-                genres = {movie.genres}
-              />
-            ))}
-          </div>
-        )}
-        <footer className="footer">
-          <p className="footer__text">Made by Yeong-Min Ko</p>
-        </footer>
-      </section>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<React.Fragment><Navigation /><Home /></React.Fragment>} />
+        <Route path="/about" element={<React.Fragment><Navigation /><About /></React.Fragment>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ minflix/src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navigation.css';
+
+function Navigation() {
+    return (
+        <div className="navigation">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/about" className="nav-link">About</Link>
+        </div>
+    );
+}
+
+export default Navigation;
